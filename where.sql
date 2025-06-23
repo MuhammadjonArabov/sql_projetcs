b19d36b5319730f5c77226d980cb074702f15727
MuhammadjonArabov <arapovmuhammadjon1@gmail.com>
2025-06-23 22:14:09 +0500

    where operator

---
+++ where.sql
@@ -0,0 +1,22 @@
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    grade FLOAT,
+    city VARCHAR(50)
+);
+
+INSERT INTO students (first_name, last_name, age, grade, city) VALUES
+('Ali', 'Karimov', 20, 85.5, 'Toshkent'),
+('Dilshod', 'Saidov', 19, 72.0, 'Andijon'),
+('Malika', 'Yuldasheva', 21, 90.0, 'Namangan'),
+('Sardor', 'Bekmurodov', 18, 66.5, 'Samarqand'),
+('Aziza', 'Shamsiyeva', 20, 48.0, 'Buxoro'),
+('Jamshid', 'Xolmatov', 22, 95.0, 'Toshkent'),
+('Zarnigor', 'Xidirova', 17, 55.5, 'Farg‘ona'),
+('Otabek', 'Qo‘chqorov', 19, 80.0, 'Xorazm'),
+('Shahnoza', 'Rustamova', 20, 77.7, 'Toshkent'),
+('Bobur', 'Nasriddinov', 23, 60.0, 'Qashqadaryo'),
+('Muhammad', 'Salimov', 21, 82.2, 'Andijon'),
+('Laylo', 'Azimova', 19, 68.4, 'Namangan');
