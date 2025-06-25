5420f50e70b8609ecd4702646318a24067ca2eea
MuhammadjonArabov <arapovmuhammadjon1@gmail.com>
2025-06-25 18:09:31 +0500

    intermediate questions sql solutions

--- simple_questions_solutions.sql
+++ simple_questions_solutions.sql
@@ -24,59 +24,77 @@
 ----------- 1 ---------
 SELECT * FROM students;
 
+
 ----------- 2 ---------
 SELECT * FROM students WHERE city = 'Toshkent'
+
 
 ----------- 3 ---------
 SELECT * FROM students WHERE age > 20;
 
+
 ----------- 4 ---------
 SELECT * FROM students WHERE grade > 80;
+
 
 ----------- 5 ---------
 SELECT * FROM students LIMIT 5;
 
+
 ----------- 6 ---------
 SELECT * FROM students ORDER BY age ASC;
+
 
 ----------- 7 ---------
 SELECT * FROM students ORDER BY grade DESC;
 
+
 ----------- 8 ---------
 SELECT COUNT(*) AS total_students FROM students;
+
 
 ----------- 9 ---------
 SELECT MIN(age) AS youngest_students FROM students;
 
+
 ----------- 10 --------
 SELECT MAX(grade) AS max_grade FROM students;
+
 
 ----------- 11 --------
 SELECT SUM(grade) AS sum_grade FROM students;
 
+
 ----------- 12 --------
 SELECT AVG(grade) AS average_grade FROM students;
 
+
 ----------  13 --------
 SELECT * FROM students WHERE first_name LIKE 'A%';
+
 
 ---------- 14 ----------
 SELECT * FROM students WHERE city = 'Toshkent' OR city = 'Andijon';
 SELECT * FROM starts WHERE city IN ('Toshkent', 'Andijon');
 
+
 ---------- 15 ----------
 SELECT * FROM students WHERE age BETWEEN 18 AND 20;
 SELECT * FROM students WHERE age >= 18 AND age <= 20;
+
 
 ---------- 16 ----------
 SELECT * FROM students WHERE NOT city = 'Toshkent';
 SELECT * FROM student WHERE city != 'Toshkent';
 
+
 ---------- 17 ----------
 SELECT * FROM students WHERE city = 'Toshkent' AND grade > 70;
 
+
 --------- 18 -----------
 SELECT * FROM students WHERE grade < 60 OR grade > 90;
+
 
 --------- 20 -----------
 SELECT * FROM students ORDER BY grade DESC LIMIT 3;
