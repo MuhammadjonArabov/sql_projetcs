8d9cd673842af127afe0d7da5f5a15e8c3455c53
MuhammadjonArabov <arapovmuhammadjon1@gmail.com>
2025-06-25 19:21:26 +0500

    intermediate questions sql solutions

--- intermediate_questions_solution.sql
+++ intermediate_questions_solution.sql
@@ -6,7 +6,7 @@
 5. Calculate the average age of students from Namangan.
 6. Calculate the sum of grades for students aged 20.
 7. Select students whose last name ends with 'ov' or 'ova'.
-8. Select students from Samarqand or Buxoro with grades above 50.
+8. Select students from Samarqand or Buxoro with grades above 40.
 9. Select students not from Toshkent or Andijon.
 10. Select students with grades between 70 and 90, sorted by age.
 11. Count students with first names containing 'sh'.
@@ -23,3 +23,44 @@
 
 ---------- 1 ----------
 SELECT * FROM WHERE city = 'Toshkent' ORDER BY grade DESC;
+
+
+---------- 2 ----------
+SELECT COUNT(*) FROM students WHERE age > 19;
+
+
+---------- 3 ----------
+SELECT MIN(grade) AS min_grade_in_andijan FROM students WHERE city = 'Toshkent';
+
+
+---------- 4 ----------
+SELECT MAX(age) AS max_age FROM students WHERE age > 80;
+
+
+---------- 5 ----------
+SELECT AVG(age) AS avg_age FROM students WHERE city = 'Nmanagan';
+
+
+---------- 6 ----------
+SELECT SUM(grade) AS sum_grade FROM students WHERE age = 20;
+
+
+---------- 7 ----------
+SELECT * FROM students WHERE last_name LIKE '%ov' OR last_name LIKE '%ova';
+
+
+---------- 8 ----------
+SELECT * FROM students WHERE city IN ('Samarqand', 'Buxoro') AND grade > 40;
+
+
+---------- 9 ----------
+SELECT * FROM students WHERE city NOT IN ('Toshkent', 'Andijon');
+
+
+---------- 10 ----------
+SELECT * FROM students WHERE grade BETWEEN 70 AND 90 ORDER BY age ASC;
+
+
+---------- 11 ----------
+SELECT COUNT(*) AS count_sh FROM students WHERE first_name LIKE '%sh%';
+
