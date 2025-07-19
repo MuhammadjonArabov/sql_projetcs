486b416c680a31d45b5be24bc484341844ea1861
MuhammadjonArabov <arapovmuhammadjon1@gmail.com>
2025-07-19 21:37:02 +0500

    sql queries

---
+++ sql_queries/easy_queries.sql
@@ -0,0 +1,41 @@
+/**
+Barcha xodimlarning ismlari va maoshlarini chiqaring.
+IT bo‘limidagi xodimlar ro‘yxatini chiqaring.
+Maoshi 4000 dan yuqori bo‘lgan xodimlarni tartiblangan holda chiqaring.
+Eng yuqori maosh oluvchi xodimni toping.
+Oxirgi 3 yilda ishga qabul qilingan xodimlarni chiqaring.
+Ismi 'A' harfi bilan boshlanadigan xodimlarni chiqaring.
+Toshkentda joylashgan bo‘limlarni chiqaring.
+Barcha loyihalarni boshlanish sanasi bo‘yicha tartiblang.
+Eng past maosh oluvchi xodimni toping.
+Farg‘ona yoki Andijon bo‘limlarida ishlaydigan xodimlarni chiqaring.
+Har bir bo‘limdagi xodimlar sonini chiqaring.
+Barcha xodimlarning umumiy maoshini hisoblang.
+IT bo‘limidagi xodimlarning o‘rtacha maoshini toping.
+Eng katta byudjetli loyihani toping.
+Har bir bo‘limdagi loyihalarning umumiy byudjetini chiqaring.
+Kamida 2 ta xodimi bor bo‘limlarni chiqaring.
+Eng kichik loyiha byudjetini toping.
+Har bir bo‘limdagi eng yuqori maoshni chiqaring.
+2023-yilda boshlangan loyihalar sonini toping.
+Har bir bo‘limdagi o‘rtacha maoshni va xodimlar sonini chiqaring.
+Har bir xodimning bo‘limi nomini chiqaring.
+IT bo‘limidagi loyihalar va ularning boshlanish sanasini chiqaring.
+Hech qanday loyihasi bo‘lmagan bo‘limlarni chiqaring.
+Barcha bo‘limlar va ulardagi xodimlar ro‘yxatini chiqaring (xodimi bo‘lmasa ham).
+Xodimlari va loyihalari bo‘lgan bo‘limlarni chiqaring.
+IT va HR bo‘limlaridagi xodimlar va loyihalarni birlashtiring.
+Barcha loyihalarni va ularning bo‘lim nomlarini chiqaring.
+Maoshi 5000 dan yuqori bo‘lgan xodimlarning bo‘limlarini chiqaring.
+Har bir loyiha uchun bo‘lim joylashuvini chiqaring.
+Xodimlari bo‘lmagan bo‘limlarni chiqaring.
+**/
+
+
+-------- 1 ---------
+SELECT first_name, salary FROM employees;
+
+-------- 2 ---------
+SELECT * FROM employees WHERE deparment_id = 1;
+
+-------- 3 ---------
